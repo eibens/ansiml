826b7cd159d518e0ee3fd0fa3b567bec409c6e22
eibens <l.eibensteiner@gmail.com>
2021-06-14 20:38:27 +0200

    :hammer: build: simplify build

--- build.sh
+++ build.sh
@@ -1,18 +1,33 @@
+#!/bin/bash
+
 # Exit if one command fails.
 set -e
+
+COV_DIR=".cov"
+COV_FILE="mod.lcov"
 
 # Lint and format.
 deno lint
 deno fmt --check
 
 # Run tests and generate coverage profile.
-deno test -A --unstable --coverage=.cov
+deno test -A --unstable --coverage=$COV_DIR
 
 # Print coverage info to stdout.
-deno coverage --unstable .cov
+deno coverage --unstable $COV_DIR
 
-# Generate coverage file.
-deno coverage --lcov .cov > mod.lcov
+# Only run this from GitHub Actions.
+if [ "$CI" = true ]; then
 
-# Delete coverage profile.
-rm -rf .cov
+  # Generate coverage file.
+  deno coverage --lcov $COV_DIR > $COV_FILE
+
+  # Upload coverage file.
+  bash <(curl -s https://codecov.io/bash) -f $COV_FILE
+
+  # Delete coverage file.
+  rm -f $COV_FILE
+fi
+
+# Delete coverage data
+rm -rf $COV_DIR
