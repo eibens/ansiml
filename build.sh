bc5e79179be807bfe60da912c31803334328dc38
eibens <l.eibensteiner@gmail.com>
2021-06-14 19:24:45 +0200

    :hammer: build: simplify build

--- build.sh
+++ build.sh
@@ -3,36 +3,16 @@ set -e
 
 # Lint and format.
 deno lint
-deno fmt
+deno fmt --check
 
 # Run tests and generate coverage profile.
-deno test \
--A \
---unstable \
---coverage=.cov
+deno test -A --unstable --coverage=.cov
 
 # Print coverage info to stdout.
-deno coverage \
---unstable \
-.cov
+deno coverage --unstable .cov
 
 # Generate coverage file.
-deno coverage \
---lcov \
-.cov \
-> mod.lcov
+deno coverage --lcov .cov > mod.lcov
 
 # Delete coverage profile.
 rm -rf .cov
-
-# Generate bundle
-deno bundle mod.ts mod.js
-deno fmt mod.js
-
-# The developer must ensure that all files are updated.
-# Therefore, we fail if there are uncommitted changes.
-status=$(git status --porcelain)
-if [ -n "$status" ]; then
-  echo "Repository contains changed files."
-  exit 1
-fi
