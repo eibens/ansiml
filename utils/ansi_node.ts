e629c7fe384a2bc88ed27bdb0eb2f969eac8a737
Lukas Eibensteiner <l.eibensteiner@gmail.com>
2023-05-21 21:19:12 +0200

    refactor: simplify types

--- utils/ansi_node.ts
+++ utils/ansi_node.ts
@@ -1,8 +1,10 @@
 import type * as ANSI from "./ansi_transformers.ts";
 import type { Command, Node } from "./stringify.ts";
 
+/** MAIN **/
+
 export type AnsiTransformers = typeof ANSI;
 
-export type AnsiNode = Node<typeof ANSI>;
+export type AnsiNode = Node<AnsiTransformers>;
 
-export type AnsiCommand = Command<typeof ANSI, string, string>;
+export type AnsiCommand = Command<AnsiTransformers, string, string>;
